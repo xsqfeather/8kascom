936191e4e5fe29145a39130209f9ceef437da1db
Simon 徐 <simonxu@SimondeMacBook-Pro.local>
2023-09-15 16:02:27 +0800

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,45 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ThemeRegistry from '@/components/ThemeRegistry/ThemeRegistry';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ThemeRegistry from "@/components/ThemeRegistry/ThemeRegistry";
+import { ButtonBase } from "@mui/material";
+import Link from "next/link";
+import DrawerToggler from "@/components/DrawerToggler";
+export const runtime = "edge";
 
 export const metadata = {
-  title: '8Kas Articles',
-  description: 'Causal Inference, Machine Learning, and Software Engineering',
+  title: "8Kas Articles",
+  description: "Causal Inference, Machine Learning, and Software Engineering",
 };
 
-
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
         <ThemeRegistry>
-          <AppBar position="fixed" sx={{ zIndex: 2000 }}>
-            <Toolbar sx={{ backgroundColor: 'background.paper' }}>
-              <DashboardIcon sx={{ color: '#444', mr: 2, transform: 'translateY(-2px)' }} />
-              <Typography variant="h6" noWrap component="div" color="black">
-               8Kas Articles
-              </Typography>
+          <AppBar position="fixed" sx={{ zIndex: 50 }}>
+            <Toolbar sx={{ backgroundColor: "background.paper" }}>
+              <DrawerToggler />
+              <ButtonBase LinkComponent={Link} href="/">
+                <Typography variant="h6" noWrap component="div" color="black">
+                  8Kas Articles
+                </Typography>
+              </ButtonBase>
             </Toolbar>
           </AppBar>
           <Box
             component="main"
             sx={{
               flexGrow: 1,
-              bgcolor: 'background.default',
-              mt: ['48px', '56px', '64px'],
+              bgcolor: "background.default",
+              mt: ["48px", "56px", "64px"],
               p: 3,
             }}
           >
